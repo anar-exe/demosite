6a68e7a5bb949b29ff407c143b66f9415805f334
Anar <anarq2006q@proton.me>
2025-08-12 15:30:45 +0000

    asd

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $(document).on("keypress", function(e) {
         if (e.which == 13) {
-            let url = `https://apidemo-chi.vercel.app/api/news`;
+            let url = `https://apidemo-pink.vercel.app/api/news`;
             async function mynews() {
                 let responce = await fetch(url)
                 let data = await responce.json();
